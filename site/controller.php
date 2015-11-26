bfd3b17be254dd13b6aea6decf37777e7e181241
daizm2000 <258630830@qq.com>
2015-11-26 22:27:40 +0800

    Update controller.php

--- site/controller.php
+++ site/controller.php
@@ -1,1 +1,58 @@
+<?php
+
+/**
+
+ * @package    Joomla.Tutorials
+
+ * @subpackageComponents
+
+ * @linkhttp://docs.joomla.org/Developing_a_Model-View-Controller_Component_-_Part_1
+
+ * @license    GNU/GPL
+
+ */
+
+// No direct access
+
+defined( '_JEXEC' ) or die( 'Restricted access' );
+
+jimport('joomla.application.component.controller');
+
+ 
+
+/**
+
+ * Hello WorldComponent Controller
+
+ *
+
+ * @package    Joomla.Tutorials
+
+ * @subpackageComponents
+
+ */
+
+classHelloController extends JController
+
+{
+
+    /**
+
+     * Method todisplay the view
+
+     *
+
+     * @access    public
+
+     */
+
+    functiondisplay()
+
+    {
+
+        parent::display();
+
+    }
+
+}
 
