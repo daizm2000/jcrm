5647ff3efdf9b4e8fa976ac51830d88805260e50
daizm2000 <258630830@qq.com>
2015-11-26 22:26:59 +0800

    Update hello.php

--- site/hello.php
+++ site/hello.php
@@ -1,2 +1,64 @@
+<?php
 
-hello;
+/**
+
+ * @package   Joomla.Tutorials
+
+ * @subpackage Components
+
+ * components/com_hello/hello.php
+
+ * @linkhttp://docs.joomla.org/Developing_a_Model-View-Controller_Component_-_Part_1
+
+ * @license   GNU/GPL
+
+*/
+
+// No direct access
+
+defined( '_JEXEC' ) or die( 'Restricted access' );
+
+// Require the basecontroller
+
+require_once( JPATH_COMPONENT.DS.'controller.php' );
+
+ 
+
+// Require specificcontroller if requested
+
+if($controller = JRequest::getWord('controller')) {
+
+    $path = JPATH_COMPONENT.DS.'controllers'.DS.$controller.'.php';
+
+    if (file_exists($path)) {
+
+        require_once $path;
+
+    } else {
+
+        $controller = '';
+
+    }
+
+}
+
+ 
+
+// Create thecontroller
+
+$classname    = 'HelloController'.$controller;
+
+$controller   = new $classname( );
+
+ 
+
+// Perform the Requesttask
+
+$controller->execute( JRequest::getWord( 'task' ) );
+
+ 
+
+// Redirect if set bythe controller
+
+$controller->redirect();
+
