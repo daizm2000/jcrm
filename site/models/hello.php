1d27bb177080bd861db80496f2987a1a1bb706b2
daizm2000 <258630830@qq.com>
2015-11-26 22:43:28 +0800

    Update hello.php

--- site/models/hello.php
+++ site/models/hello.php
@@ -1,1 +1,60 @@
 
+<?php
+
+/**
+
+ * Hello Model forHello World Component
+
+ *
+
+ * @package    Joomla.Tutorials
+
+ * @subpackageComponents
+
+ * @linkhttp://docs.joomla.org/Developing_a_Model-View-Controller_Component_-_Part_2
+
+ * @license    GNU/GPL
+
+ */
+
+// No direct access
+
+defined( '_JEXEC' ) or die( 'Restricted access' );
+
+jimport( 'joomla.application.component.model' );
+
+ 
+
+/**
+
+ * Hello Model
+
+ *
+
+ * @package    Joomla.Tutorials
+
+ * @subpackageComponents
+
+ */
+
+classHelloModelHello extends JModel
+
+{
+
+    /**
+
+    * Gets thegreeting
+
+    * @returnstring The greeting to be displayed to the user
+
+    */
+
+    functiongetGreeting()
+
+    {
+
+        return 'Hello,World!';
+
+    }
+
+}
