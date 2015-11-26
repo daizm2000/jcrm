159848e804e842d9a8a7db14335c94f2fe693a61
daizm2000 <258630830@qq.com>
2015-11-26 22:29:11 +0800

    Update view.html.php

--- site/views/hello/view.html.php
+++ site/views/hello/view.html.php
@@ -1,1 +1,50 @@
+<?php
+
+/**
+
+ * @package    Joomla.Tutorials
+
+ * @subpackageComponents
+
+ * @linkhttp://docs.joomla.org/Developing_a_Model-View-Controller_Component_-_Part_1
+
+ * @license    GNU/GPL
+
+*/
+
+// no direct access
+
+defined( '_JEXEC' ) or die( 'Restricted access' );
+
+jimport( 'joomla.application.component.view');
+
+ 
+
+/**
+
+ * HTML View classfor the HelloWorld Component
+
+ *
+
+ * @package    HelloWorld
+
+ */
+
+classHelloViewHello extends JView
+
+{
+
+    functiondisplay($tpl = null)
+
+    {
+
+        $greeting = "HelloWorld!";
+
+        $this->assignRef( 'greeting', $greeting );
+
+        parent::display($tpl);
+
+    }
+
+}
 
